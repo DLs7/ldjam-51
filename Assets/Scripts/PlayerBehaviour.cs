b1339d7efc2837edf96503deeaff6990f74bca2d
DLs7 <agasta@inf.ufsm.br>
2022-09-30 23:29:51 -0300

    Add movement complexion

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,19 @@ public class PlayerBehaviour : MonoBehaviour
     public float MovementSpeed;
     public Rigidbody2D Rigidbody2D;
     private Vector2 MovementDirection;
-    
+
+    private float ActiveMovementSpeed;
+    public float DashSpeed;
+    public float DashLength = .5f, DashCooldown = 1f;
+
+    private float DashCounter;
+    private float DashCoolCounter;
+
+    private void Start()
+    {
+        ActiveMovementSpeed = MovementSpeed;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -22,10 +35,33 @@ public class PlayerBehaviour : MonoBehaviour
     void GetInputs()
     {
         MovementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (DashCounter <= 0 && DashCoolCounter <= 0)
+            {
+                ActiveMovementSpeed = DashSpeed;
+                DashCounter = DashLength;
+            }
+        }
     }
 
     void Move()
     {
-        Rigidbody2D.velocity = new Vector2(MovementDirection.x * MovementSpeed, MovementDirection.y * MovementSpeed);
+        Rigidbody2D.velocity = new Vector2(MovementDirection.x * ActiveMovementSpeed, MovementDirection.y * ActiveMovementSpeed);
+
+        if (DashCounter > 0)
+        {
+            DashCounter -= Time.fixedDeltaTime;
+            if (DashCounter <= 0)
+            {
+                ActiveMovementSpeed = MovementSpeed;
+                DashCoolCounter = DashCooldown;
+            }
+        }
+
+        if (DashCoolCounter > 0)
+        {
+            DashCoolCounter -= Time.fixedDeltaTime;
+        }
     }
 }
